a30f4e96cf0e7ff3025f8ce179e600064b3da9d9
NoTreblee <42041720+NoTreblee@users.noreply.github.com>
2022-06-08 23:29:41 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,4 +16,6 @@ document.getElementById('get-location').addEventListener('click', getLocation);
 function success(position) {
   document.getElementById('output1').innerHTML = position.coords.latitude;
   document.getElementById('output2').innerHTML = position.coords.longitude;
+  document.getElementById('span1').innerHTML =
+    'URI źródła elementu „script” jest niedozwolony w tym dokumencie:';
 }
