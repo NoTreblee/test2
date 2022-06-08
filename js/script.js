b224397228296892b4f5521559de30f504ed2dcc
NoTreblee <42041720+NoTreblee@users.noreply.github.com>
2022-06-08 23:22:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function getLocation() {
+  if (!navigator.geolocation) {
+    console.log('Geolocation API not supported by this browser.');
+  } else {
+    console.log('Checking location...');
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+function success(position) {
+  console.log(position);
+}
+function error() {
+  console.log('Geolocation error!');
+}
+document.getElementById('get-location').addEventListener('click', getLocation);
+function success(position) {
+  document.getElementById('output1').innerHTML = position.coords.latitude;
+  document.getElementById('output2').innerHTML = position.coords.longitude;
+}
